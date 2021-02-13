44ff40ff130e527df548c329e2cd4e6c669816a5
Marcel Kämper <marcelkaemper862@gmail.com>
2021-02-13 23:35:15 +0100

    Added pawn movement logic

--- src/pieces/Pawn.java
+++ src/pieces/Pawn.java
@@ -14,7 +14,21 @@ public class Pawn extends Piece {
     @Override
     public boolean validMove(Cell toPosition) {
         System.out.println("checking validity of move");
-        return true;
+        if (getPosition().getCoord_x() == toPosition.getCoord_x()) {
+            int coordDelta = getPosition().getCoord_y() - toPosition.getCoord_y();
+
+            if (getColor() == EnumColor.WHITE) {
+                coordDelta *= -1;
+            }
+
+            // FIXME: Add Condition to check for toPosition-1 (or +1 for white) in case of coordDelta == 2
+            if (coordDelta == 2 && !getHasMoved() && toPosition.getPiece() == null) {
+                return true;
+            } else if (coordDelta == 1 && toPosition.getPiece() == null) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
