8bc26b3be8744b818f4788b2874019eb2ab26620
Exarous <csincay@yahoo.com>
2021-02-19 23:51:30 +0100

    Outsourced piece initialization

--- src/main/Chess.java
+++ src/main/Chess.java
@@ -1,7 +1,6 @@
 package main;
 
-import pieces.*;
-import statics.Texture;
+import pieces.Piece;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -98,60 +97,7 @@ public class Chess extends JPanel {
                 if (Cell.cells.size() < 64) {
                     EnumColor color = g.getColor().toString().equals("DARK_GRAY") ? EnumColor.BLACK : EnumColor.WHITE;
                     Cell cell = new Cell(x, y, color, letters[j], (8 - i), rect, null);
-
-                    // Pawn
-                    if (8 - i == 2) {
-                        Piece p = new Pawn(Texture.pawn_white, cell, false, EnumColor.WHITE, EnumPiece.PAWN, false, false);
-                        cell.setPiece(p);
-                    } else if (8 - i == 7) {
-                        Piece p = new Pawn(Texture.pawn_black, cell, false, EnumColor.BLACK, EnumPiece.PAWN, false, false);
-                        cell.setPiece(p);
-                    }
-
-                    // Rook
-                    if (8 - i == 1 && (letters[j] == 'a' || letters[j] == 'h')) {
-                        Piece p = new Rook(Texture.rook_white, cell, false, EnumColor.WHITE, EnumPiece.ROOK);
-                        cell.setPiece(p);
-                    } else if (8 - i == 8 && (letters[j] == 'a' || letters[j] == 'h')) {
-                        Piece p = new Rook(Texture.rook_black, cell, false, EnumColor.BLACK, EnumPiece.ROOK);
-                        cell.setPiece(p);
-                    }
-
-                    // Queen
-                    if (8 - i == 1 && (letters[j] == 'd')) {
-                        Piece p = new Queen(Texture.queen_white, cell, false, EnumColor.WHITE, EnumPiece.QUEEN);
-                        cell.setPiece(p);
-                    } else if (8 - i == 8 && (letters[j] == 'd')) {
-                        Piece p = new Queen(Texture.queen_black, cell, false, EnumColor.BLACK, EnumPiece.QUEEN);
-                        cell.setPiece(p);
-                    }
-
-                    // Bishop
-                    if (8 - i == 1 && (letters[j] == 'c' || letters[j] == 'f')) {
-                        Piece p = new Bishop(Texture.bishop_white, cell, false, EnumColor.WHITE, EnumPiece.BISHOP);
-                        cell.setPiece(p);
-                    } else if (8 - i == 8 && (letters[j] == 'c' || letters[j] == 'f')) {
-                        Piece p = new Bishop(Texture.bishop_black, cell, false, EnumColor.BLACK, EnumPiece.BISHOP);
-                        cell.setPiece(p);
-                    }
-
-                    // Knight
-                    if (8 - i == 1 && (letters[j] == 'b' || letters[j] == 'g')) {
-                        Piece p = new Knight(Texture.knight_white, cell, false, EnumColor.WHITE, EnumPiece.KNIGHT);
-                        cell.setPiece(p);
-                    } else if (8 - i == 8 && (letters[j] == 'b' || letters[j] == 'g')) {
-                        Piece p = new Knight(Texture.knight_black, cell, false, EnumColor.BLACK, EnumPiece.KNIGHT);
-                        cell.setPiece(p);
-                    }
-
-                    // King
-                    if (8 - i == 1 && letters[j] == 'e') {
-                        Piece p = new King(Texture.king_white, cell, false, EnumColor.WHITE, EnumPiece.KING);
-                        cell.setPiece(p);
-                    } else if (8 - i == 8 && letters[j] == 'e') {
-                        Piece p = new King(Texture.king_black, cell, false, EnumColor.BLACK, EnumPiece.KING);
-                        cell.setPiece(p);
-                    }
+                    Piece.createInitPiece(cell, i, j, letters);
                 }
                 g.setColor(Color.BLACK);
                 g.drawChars(letters, j, 1, (25 * (j + 1) + (25 * j)) + 28, 420);
